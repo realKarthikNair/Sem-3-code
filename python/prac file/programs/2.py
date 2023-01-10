8d0ca2b5b9f6b2965f1cb6df5d21dc258ddc53e0
realkarthiknair <knairothera@gmail.com>
2023-01-10 15:14:00 +0530

    improved prac 2

--- python/prac file/programs/2.py
+++ python/prac file/programs/2.py
@@ -2,8 +2,19 @@
 For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. 
 The character’s position doesn’t matter."""
 
+# Cool but not memory efficient for large strings
+# def balanced(s1, s2):
+#     return [False if i not in s2 else True for i in s1].count(False)==0
+
+# again not memory efficient if strings are big
+# def balanced(s1, s2):
+#     return set(s1).issubset(set(s2))
+
 def balanced(s1, s2):
-    return [True if i in s2 else False for i in s1].count(False)==0
+    for i in s1:
+        if i not in s2:
+            return False
+    return True
 
 if __name__=="__main__":
     print(balanced("abc", "cba"))
