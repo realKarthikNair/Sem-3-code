92f6721565854c7b5619e43e77d53c3cba57c969
realkarthiknair <knairothera@gmail.com>
2023-01-10 14:47:30 +0530

    prac qn 1

---
+++ python/prac file/programs/1.py
@@ -0,0 +1,19 @@
+"""    1. Write a program to print the following pattern using a loop.
+    1
+   212
+  32123
+ 4321234
+543212345"""
+
+
+def pattern(n):
+    for i in range(1,n+1):
+        print(" "*(n-i), end="")
+        for j in range(i,0,-1):
+            print(j, end="")
+        for k in range(2, i+1):
+            print(k, end="")
+        print()
+
+pattern(5)
+
